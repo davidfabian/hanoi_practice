916020cac0ee3e9cc1c39630aae0150ebec76664
davidfabian <fabiand.01@gmail.com>
2018-07-03 09:55:49 +0300

    added comments to MainActivity and Jatek

--- app/src/main/java/com/d/tornyok/Jatek.java
+++ app/src/main/java/com/d/tornyok/Jatek.java
@@ -10,7 +10,7 @@ import android.widget.Button;
 
 public class Jatek extends AppCompatActivity {
 
-
+    //vertical guidelines to divide the screen to three equal parts
     Guideline guidelineAB;
     Guideline guidelineBC;
 
@@ -21,47 +21,54 @@ public class Jatek extends AppCompatActivity {
         constraintLayout.setId(R.id.jatek_constraintlayout);
         setContentView(constraintLayout);
 
+//get the number of levels from MainActivity, and instantiate the guidelines
         Intent intent = getIntent();
         int levelCount = intent.getIntExtra(MainActivity.EXTRA_MESSAGE, 3);
         guidelineAB = new Guideline(this);
         guidelineBC = new Guideline(this);
-
         guidelineAB.setId(R.id.guidelineAB);
         guidelineBC.setId(R.id.guidelineBC);
-
+//call the starting layout builder method.
         createLevels(levelCount, constraintLayout);
 
     }
 
 
     public void createLevels(int levelCount, ConstraintLayout constraintLayout) {
+//create a sketch (ConstraintSet)of the layout changes
         ConstraintSet constraintSet = new ConstraintSet();
+//add first guideline, AB, at first third of the screen
         constraintSet.create(R.id.guidelineAB, ConstraintSet.VERTICAL_GUIDELINE);
         constraintSet.setGuidelinePercent(R.id.guidelineAB, 0.33f);
-
+//add second guideline, BC, at second third of the screen
         constraintSet.create(R.id.guidelineBC, ConstraintSet.VERTICAL_GUIDELINE);
         constraintSet.setGuidelinePercent(R.id.guidelineBC, 0.66f);
+//apply the constraintSet to the layout.
         constraintSet.applyTo(constraintLayout);
-
+//creates @levelCount number of buttons. Names them, positions them vertically
         for (int i = levelCount; i >= 1; i--) {
             Button btn = new Button(this);
             btn.setText(String.format("%d", i));
             btn.setId(i);
             final int id_ = btn.getId();
-//            btn.setLayoutParams(new ConstraintLayout.LayoutParams(
-//                    ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+//add button to the layout
             constraintLayout.addView(btn);
             constraintSet.clone(constraintLayout);
+//position it to the first third of the screen. buttons left side constrained to screen left side
             constraintSet.connect(btn.getId(), ConstraintSet.LEFT, constraintLayout.getId(), ConstraintSet.LEFT);
+//position it to the first third of the screen. buttons right side constrained to AB guideline's left side
             constraintSet.connect(btn.getId(), ConstraintSet.RIGHT, guidelineAB.getId(), ConstraintSet.LEFT);
+//add the button to a group, a vertical chain spread evenly in the available space
             constraintSet.setVerticalChainStyle(btn.getId(), ConstraintSet.CHAIN_SPREAD_INSIDE);
+//the bottom level button vertically constrained to the bottom of the screen
             if (i == levelCount) {
                 constraintSet.connect(btn.getId(), ConstraintSet.BOTTOM, constraintLayout.getId(), ConstraintSet.BOTTOM);
                 constraintSet.setVerticalChainStyle(id_, ConstraintSet.BOTTOM);
+//all other buttons vertically constrained to the button below.
             } else {
                 constraintSet.connect(btn.getId(), ConstraintSet.BOTTOM, id_ + 1, ConstraintSet.TOP);
             }
-
+//apply the sketch (constraintSet)
             constraintSet.applyTo(constraintLayout);
         }
     }
--- app/src/main/java/com/d/tornyok/MainActivity.java
+++ app/src/main/java/com/d/tornyok/MainActivity.java
@@ -5,10 +5,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+//start screen. showing the number of levels, increase, decrease and start the game
 
 public class MainActivity extends AppCompatActivity {
+    //    intent id
     public static final String EXTRA_MESSAGE = "NUMBER_OF_LEVELS";
+    //    int tot store number of levels
     int levelCount;
+    //    shows a number, the number of levels
     TextView TVLevelCounter;
 
     @Override
@@ -20,12 +24,16 @@ public class MainActivity extends AppCompatActivity {
         updateCounter(levelCount);
     }
 
+    // Starts @Jatek.java, and send number of levels @levelCount in the intent
+//    called when the Start button (b_start) clicked.
     public void sendMessage(View view) {
         Intent intent = new Intent(this, Jatek.class);
         intent.putExtra(EXTRA_MESSAGE, levelCount);
         startActivity(intent);
     }
 
+    //increase number of levels by one, to a maximum of 10, then updates the levelCount variable
+//    called when plus button (b_increase) clicked.
     public void increaseLevelCount(View v) {
         if (levelCount < 10) {
             levelCount += 1;
@@ -33,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
         updateCounter(levelCount);
     }
 
+    //decrease the number of levels, to a minimum of 3, then updates the levelCount variable
+//    called when minus button (b_decrease) clicked.
     public void decreaseLevelCount(View v) {
         if (levelCount > 3) {
             levelCount -= 1;
